0eed8f1ab8305687a81c3373f1d369abfe5f3cdd
Jeremy Elizondo <braseth.16@outlook.com>
2025-06-24 17:43:57 -0600

    Agregar archivos de proyecto.

---
+++ SistemaMantenimientoAC.Core/DTOs/CitaDTO.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SistemaMantenimientoAC.Core.DTOs
+{
+    internal class CitaDTO
+    {
+    }
+}
